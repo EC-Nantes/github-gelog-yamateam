6dc62b1a18f61f87252b097f159775b95f3c0707
Yael <yael.vincent@orange.fr>
2025-09-19 17:40:04 +0200

    Add files via upload
    oui

---
+++ ProjetTP/src/main/java/org/centrale/objet/WoE/Lapin.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.centrale.objet.WoE;
+
+/**
+ *
+ * @author yaelv
+ */
+public class Lapin extends Monstre {
+
+    public Lapin(int ptVie, int degAtt, int ptPar, int pageAtt, int pagePar, Point2D p) {
+        super(ptVie, degAtt, ptPar, pageAtt, pagePar, p);
+    }
+
+    public Lapin() {
+        super();
+    }
+
+    public Lapin(Lapin l) {
+        super(l);
+    }
+    
+    
+}
---
+++ ProjetTP/src/main/java/org/centrale/objet/WoE/Monstre.java
@@ -0,0 +1,102 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.centrale.objet.WoE;
+
+import java.util.Random;
+/**
+ *
+ * @author yaelv
+ */
+public class Monstre {
+    
+    private int ptVie;
+    private int degAtt;
+    private int ptPar;
+    private int pageAtt;
+    private int pagePar;
+    private Point2D p;
+
+    public Monstre(int ptVie, int degAtt, int ptPar, int pageAtt, int pagePar, Point2D p) {
+        this.ptVie = ptVie;
+        this.degAtt = degAtt;
+        this.ptPar = ptPar;
+        this.pageAtt = pageAtt;
+        this.pagePar = pagePar;
+        this.p = p;
+    }
+
+    public Monstre() {
+        this(100,10,2,60,40,new Point2D());
+    }
+    
+    public Monstre(Monstre m) {
+        this.ptVie = m.getPtVie();
+        this.degAtt = m.getDegAtt();
+        this.ptPar = m.getPtPar();
+        this.pageAtt = m.getPageAtt();
+        this.pagePar = m.getPagePar();
+        this.p = m.getP();
+    }
+    
+
+    public int getPtVie() {
+        return ptVie;
+    }
+
+    public int getPtPar() {
+        return ptPar;
+    }
+
+    public int getPagePar() {
+        return pagePar;
+    }
+
+    public int getPageAtt() {
+        return pageAtt;
+    }
+
+    public int getDegAtt() {
+        return degAtt;
+    }
+
+    public Point2D getP() {
+        return p;
+    }
+
+    public void setPtVie(int ptVie) {
+        this.ptVie = ptVie;
+    }
+
+    public void setDegAtt(int degAtt) {
+        this.degAtt = degAtt;
+    }
+
+    public void setPtPar(int ptPar) {
+        this.ptPar = ptPar;
+    }
+
+    public void setPageAtt(int pageAtt) {
+        this.pageAtt = pageAtt;
+    }
+
+    public void setPagePar(int pagePar) {
+        this.pagePar = pagePar;
+    }
+
+    public void setP(Point2D p) {
+        this.p = new Point2D(p);
+    }
+
+    public void deplace(){
+        Random aleaInt = new Random();
+        int movex = aleaInt.nextInt(3)-1;
+        int movey = aleaInt.nextInt(3)-1;
+        p.translate(movex,movey);
+    }
+    
+    public void affiche(){
+        
+    }
+}
