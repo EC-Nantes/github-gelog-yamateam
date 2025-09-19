0deb953690da9f9e805def6879f886da01e172d9
Mathys INFOSI <56838862+Wygoo@users.noreply.github.com>
2025-09-19 17:38:05 +0200

    perso

---
+++ ProjetTP/src/main/java/org/centrale/objet/Paysan.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.centrale.objet.WoE;
+
+public class Paysan extends Personnage {
+    
+    public Paysan(String n, int pV, int dA, int pPar, int paAtt, int paPar, int dMax, Point2D p){
+        super(n,pV,dA,pPar,paAtt,paPar,dMax,p);
+    }
+    
+    public Paysan(Paysan p){
+        this(p.getNom(), p.getPtVie(),p.getDegAtt(),p.getPtPar(), p.getPageAtt(), p.getPagePar(),p.getDistAttMax(),p.getPos());
+    }
+    
+    public Paysan(){
+        this("Jean",100,50,50,75,25,3,new Point2D(0,0));  
+    }
+}
