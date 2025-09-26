6f92e41191f3608c379db10cf5f20806ae502ebd
Yael <yael.vincent@orange.fr>
2025-09-26 16:58:35 +0200

    ProjetTP.java

--- ProjetTP/src/main/java/org/centrale/objet/WoE/ProjetTP.java
+++ ProjetTP/src/main/java/org/centrale/objet/WoE/ProjetTP.java
@@ -1,16 +1,1 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
-package org.centrale.objet.WoE;
-
-/**
- *
- * @author yaelv
- */
-public class ProjetTP {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
