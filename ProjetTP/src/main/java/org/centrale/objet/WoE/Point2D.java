1620bbc4b4a1abe7706588dd08762ee8af458c89
Mathys INFOSI <56838862+Wygoo@users.noreply.github.com>
2025-09-26 17:11:40 +0200

    Update Point2D.java

--- ProjetTP/src/main/java/org/centrale/objet/WoE/Point2D.java
+++ ProjetTP/src/main/java/org/centrale/objet/WoE/Point2D.java
@@ -1,8 +1,12 @@
-    /*
+/*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 package org.centrale.objet.WoE;
 
+/**
+ *
+ * @author dytri
+ */
 public class Point2D {
 
     /**
@@ -11,6 +15,11 @@ public class Point2D {
     private int x;
     private int y;
 
+    /**
+     *
+     * @param x
+     * @param y
+     */
     public Point2D(int x, int y) {
         /**
          * Constructeur d'un point en 2D
@@ -21,12 +30,19 @@ public class Point2D {
         this.y = y;
     }
     
+    /**
+     *
+     * @param p
+     */
     public Point2D(Point2D p)
     {
-        x = p.getX();
+        x = p.getX();   
         y = p.getY();
     }
     
+    /**
+     *
+     */
     public Point2D() {
         /**
          * Construction d'un point à l'origine (un choix)
@@ -37,43 +53,78 @@ public class Point2D {
         this.y = 0 ;
     }
     
-    
+    /**
+     *
+     * @return
+     */
     public int getX() {
         return x;
     }
     
+    /**
+     *
+     * @return
+     */
     public int getY() {
         return y;
     }
     
+    /**
+     *
+     * @param x
+     */
     public void setX(int x) {
         this.x = x;
     }
     
+    /**
+     *
+     * @param y
+     */
     public void setY(int y) {
         this.y = y;
     }
 
+    /**
+     *
+     * @param x
+     * @param y
+     */
     public void setPosition(int x, int y) {
         this.x = x;
         this.y = y;
     }
     
+    /**
+     *
+     */
     public void affiche(){
         // Possibilité d'afficher le nom du point ?
         // Par exemple : le point p2 est aux coordonnées...
         System.out.println("[" + this.x + ";" + this.y + "]");
      }
     
+    /**
+     *
+     * @param dx
+     * @param dy
+     */
     public void translate(int dx, int dy){
         this.x += dx;
         this.y += dy;
     }
     
+    /**
+     * Donne la distance entre deux points du plan
+     * 
+     * @param p1    Point n°1
+     * @param p2    Point n°2
+     * 
+     * @return distance entre p1 et p2 (float)
+     */
     public double distance(Point2D p1, Point2D p2){
         double d = Math.sqrt((p1.getX()-p2.getX())*(p1.getX()-p2.getX())+(p1.getY()-p2.getY())*(p1.getY()-p2.getY()));
         return d;
     }
-    
     
 }
