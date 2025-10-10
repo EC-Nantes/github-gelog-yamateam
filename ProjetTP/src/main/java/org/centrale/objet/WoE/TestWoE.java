e98c2293c07fef3ff7a704ce2e2a48f306d37467
Yael <yael.vincent@orange.fr>
2025-10-10 16:18:44 +0200

    Update TestWoE.java

--- ProjetTP/src/main/java/org/centrale/objet/WoE/TestWoE.java
+++ ProjetTP/src/main/java/org/centrale/objet/WoE/TestWoE.java
@@ -6,28 +6,26 @@ package org.centrale.objet.WoE;
 
 /**
  *
- * @author dytri
+ * @author mathys,yaël
  */
 public class TestWoE {
-
-    /**
-     *
-     * @param args
-     */
+    
     public static void main(String[] args){
-        World w = new World(50);
-        w.creerMondeAlea();
+        
+        World w = new World(10000);
+        w.creerMondeAlea(1000000);
 
 
         long tpsDebut1 = System.nanoTime();
         int totPV = 0;
-        for(int i=0;i<100;i++){
+        for(int i=0;i<w.getListe_perso().size();i++){
             totPV += w.getListe_perso().get(i).getPtVie();
         }
         long tpsFin1 = System.nanoTime();
-        System.out.println("Temps écoulé boucle for(ns): " + (tpsFin1-tpsDebut1));
+        System.out.println("Temps écoulé (ns): " + (tpsFin1-tpsDebut1));
+        System.out.println(totPV);
 
-        
+                
         long tpsDebut2 = System.nanoTime();
         totPV = 0;
         for(Personnage p : w.getListe_perso()){
@@ -37,7 +35,6 @@ public class TestWoE {
         System.out.println("Temps écoulé boucle itéré(ns): " + (tpsFin2-tpsDebut2));
 
     }
-
-
 }
-
+    
+ 
