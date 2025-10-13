97922bf2809bcd69d7d51a25aeb1105823ec55b1
Yael <yael.vincent@orange.fr>
2025-10-13 17:03:01 +0200

    Update ElementDeJeu.java

--- ProjetTP/src/main/java/org/centrale/objet/WoE/ElementDeJeu.java
+++ ProjetTP/src/main/java/org/centrale/objet/WoE/ElementDeJeu.java
@@ -9,5 +9,33 @@ package org.centrale.objet.WoE;
  * @author yaelv
  */
 public abstract class ElementDeJeu {
+    private Point2D pos;
+    private String nom;
+
+    public ElementDeJeu(String n, Point2D p){
+        nom=n;
+        pos=p;             
+    }
+                            
+            
+    public Point2D getPos() {
+        return pos;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setPos(Point2D pos) {
+        this.pos = pos;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
     
+     /** Crée une copie indépendante de l'élément
+     * @return un élément indépendant
+     */
+    public abstract ElementDeJeu copie();    
 }
