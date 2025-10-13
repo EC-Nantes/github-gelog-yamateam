8f7580e73f8f28eb5156a36d6653b9c95005a467
Yael <yael.vincent@orange.fr>
2025-10-13 17:02:33 +0200

    Update NuageToxique.java

--- ProjetTP/src/main/java/org/centrale/objet/WoE/NuageToxique.java
+++ ProjetTP/src/main/java/org/centrale/objet/WoE/NuageToxique.java
@@ -4,10 +4,66 @@
  */
 package org.centrale.objet.WoE;
 
+import java.util.Random;
+
 /**
  *
  * @author yaelv
  */
 public class NuageToxique extends Objet implements Deplacable, Combattant {
+    
+    private int degAtt;
+    
+    private int vitesse;
+    
+    private int longueur;
+    
+    private int largeur;
+    
+   
+    public NuageToxique(int a,int v,int l1,int l2){
+        degAtt=a;
+        vitesse=v;
+        longueur=l1;
+        largeur=l2;
+    }
+    
+    public NuageToxique(){
+        this(1,2,5,4);
+    }
+    
+    
+    
+    /** Crée une copie indépendante de la créature 
+     * @return un archer
+     */
+    @Override
+    public NuageToxique copie(){
+        return new NuageToxique();
+    }
+    
+    /**
+     *
+     */
+    @Override
+    public void deplacer() {
+        System.out.println("===== "+ this.getNom() +" se déplace =====");
+        int dx, dy;
+        Random randInt = new Random();
+        do { //évite le non déplacement et permet de se déplacer aléatoirement parmis les 8 cases autours de la créature
+            dx = randInt.nextInt(3);
+            dy = randInt.nextInt(3);
+        } while (dx == 1 && dy == 1);
+
+        getPos().translate(vitesse*(dx - 1),vitesse*(dy - 1));
+        System.out.println("Nouvelle position : " + this.getPos().toString());
+    }
+    
+    
+
+    @Override
+    public void combattre(Creature c) {
+        c.subirDegat(this.degAtt);
+    }
     
 }
