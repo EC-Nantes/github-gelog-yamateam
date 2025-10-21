bcac84a7ac169a7b54c9b83159b622a00b03044a
Yael <yael.vincent@orange.fr>
2025-10-21 23:24:25 +0200

    Update Paysan.java

--- ProjetTP/src/main/java/org/centrale/objet/WoE/Paysan.java
+++ ProjetTP/src/main/java/org/centrale/objet/WoE/Paysan.java
@@ -1,3 +1,4 @@
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -5,28 +6,28 @@
 package org.centrale.objet.WoE;
 
 /**
- *
+ * Type de personnage non jouable, ne combat pas
  * @author yaelv
  */
 public class Paysan extends Personnage {
     
     /**
-     *
-     * @param n
-     * @param pV
-     * @param dA
-     * @param pPar
-     * @param paAtt
-     * @param paPar
-     * @param dMax
-     * @param p
+     * Constructeur principal
+     * @param n String son nom
+     * @param pV int 
+     * @param dA int dégâts d'attaque
+     * @param pPar int points de parade
+     * @param paAtt int pourcentage de réussite d'attaque
+     * @param paPar int pourcentage de réussite de parade
+     * @param dMax int distance maximale d'attaque
+     * @param p Point2D sa position
      */
     public Paysan(String n, int pV, int dA, int pPar, int paAtt, int paPar, int dMax, Point2D p){
         super(n,pV,dA,pPar,paAtt,paPar,dMax,p);
     }
     
     /**
-     *
+     * Constructeur de copie
      * @param p
      */
     public Paysan(Paysan p){
@@ -34,12 +35,16 @@ public class Paysan extends Personnage {
     }
     
     /**
-     *
+     * Constructeur par défaut
      */
     public Paysan(){
         this("Jean",100,50,50,75,25,3,new Point2D(0,0));  
     }
     
+    /**
+     * Constructeur en initialisant seulement son nom
+     * @param nom
+     */
     public Paysan(String nom){
         this(nom,100,50,50,75,25,3,new Point2D(0,0));  
     }
