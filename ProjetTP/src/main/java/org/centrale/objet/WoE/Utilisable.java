6a5b653d284ad03a3ca9513b3e578c1941ea60d1
Mathys INFOSI <56838862+Wygoo@users.noreply.github.com>
2025-10-21 23:29:21 +0200

    Update Utilisable.java

--- ProjetTP/src/main/java/org/centrale/objet/WoE/Utilisable.java
+++ ProjetTP/src/main/java/org/centrale/objet/WoE/Utilisable.java
@@ -4,17 +4,37 @@
  */
 package org.centrale.objet.WoE;
 
-/**
+/**Interface indiquant les classes qui peuvent être récupérés par le joueur 
  *
- * @author dytri
+ * @author yaelv, mathys
  */
 public interface Utilisable {  
+     /**Applique l'effet de l'objet au perso
+     * 
+     * @param perso
+     * @param world
+     */
     public void utiliser(Personnage perso, World world);
+    
+    /**Retire l'effet de l'objet au perso
+     * 
+     * @param perso
+     */
     public void retirer(Personnage perso);
+   
+    /** Dis si l'effet de l'objet est expiré ou non selon le numéro du round
+     * 
+     * @param world
+     * @return bool_expiration
+     */
     public boolean estExpirer(World world);
     public String getNom();
     public String getCaracteristique();
     public int getEffet();
+    /**
+     * Fais une copie indépendante de l'objet
+     * @return utilisable_copie
+     */
     public Utilisable copie();
     public void setPos(Point2D pos);
     
